fb00de54b912f0e03cd6985f2bead6c83f41706d
satya ghanta <satyapawan1987@gmail.com>
2017-03-04 01:45:10 +0530

    modified batch file

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,16 @@
 @echo off
-echo Welcome to Heroku production deployment
-echo Enter loginid and password as phani590@gmail.com/Phani@590
+echo Welcome to Heroku production deployment!!!
+echo ####Enter loginid and password as phani590@gmail.com/Phani@590
 heroku login
-git pull
-gradlew -Pprod bootRepackage
+echo #### Successfully logged into Heroku ####
+git pull origin master
+echo #### Pulled changes from Git Master ####
+call gradlew -Pprod bootRepackage
+echo #### Succesfully prepared war and started deploying to Heroku cloud ####
 cd build\libs
-heroku war:deploy prod-jhips-test-0.0.1-SNAPSHOT --app prod-jhips-test
+echo #### Starting deployment ####
+call heroku war:deploy prod-jhips-test-0.0.1-SNAPSHOT.war --app prod-jhips-test
+echo #### Succefully deployed war into Heroku cloud ####
+heroku open --app prod-jhips-test
+echo #### check browser to see the application ####
+pause
